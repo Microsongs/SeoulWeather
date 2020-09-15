853095df7c0a5ea795415b757f387eba5fe76e43
현석 송 <jhn06046@hotmail.com>
2020-09-15 11:33:00 +0900

    홈페이지 틀 작성, JS실행

--- exServer.js
+++ exServer.js
@@ -21,5 +21,7 @@ let server = app.listen(3000, function(){
     console.log("Express server has started on port 3000");
 });
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
+app.use(express.static('public/css'));
 app.use('/static', express.static(__dirname + '/public/css'));
+app.use('/static', express.static(__dirname + '/public'));
